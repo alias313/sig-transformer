038b0483100d257fb084be64d910e8b23babd4a9
alias313 <eliesgarciaalvira@gmail.com>
2024-03-01 17:38:41 +0100

    Setup of charts and initial configuration

---
+++ src/signals/dataFetching.js
@@ -0,0 +1,23 @@
+export async function getData() {
+  const response = await fetch("/src/signals/fft_out.csv");
+  const fftString = await response.text();
+  const outputSignal = [];
+  const inputSignal = [];
+
+  const fftTable = fftString.split("\n").slice(1);
+  fftTable.forEach((row) => {
+    const columns = row.split(",");
+    outputSignal.push({
+      time: parseFloat(columns[0]),
+      value: parseFloat(columns[1]),
+    });
+    inputSignal.push({
+      time: parseFloat(columns[3]),
+      value: parseFloat(columns[4]),
+    });
+  });
+
+  console.log(inputSignal, outputSignal);
+
+  return { inputSignal, outputSignal };
+}
