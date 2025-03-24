ceb54ce042438a575d1c728a9c5e3e02d6c132bf
Elies <eliesgarciaalvira@gmail.com>
2025-03-24 18:47:50 +0100

    This is all v0's fault don't blame me
    Don't shoot the messenger

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,19 @@
 module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}'],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          darkest: '#050508',
+          darker: '#0d0d12',
+          dark: '#13131a',
+          darkpurple: {
+            700: '#4c1d95',
+            800: '#3b0764',
+            900: '#2e0052'
+          }
+        }
+
+      },
     },
     plugins: [],
   }
