32963ffde331f4166c7665d30a344a5814aa2cf5
Elies <eliesgarciaalvira@gmail.com>
2025-03-25 17:03:26 +0100

    Fix deleted DB but with polling
    When deleting the datadabase it took two reloads for the charts to appearbecause the charts tried to load before the DB was filled

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,9 +1,33 @@
 import Dexie from 'dexie';
 
-// Create a new database
-const db = new Dexie('SignalDB');
-db.version(1).stores({
-  signals: 'Freq'
-});
+async function createDB() {
+  // Create a new database
+  const db = new Dexie('SignalDB');
+  db.version(1).stores({
+    signals: 'Freq'
+  });
+  return db;
+}
 
-export default db;
+async function loadJSONToIndexedDB(jsonData) {
+  try {
+    const db = new Dexie('SignalDB');
+    db.version(1).stores({
+      signals: 'Freq'
+    });  
+    const data = jsonData;
+    console.log('Data loaded from JSON file into IndexedDB:');
+    console.log(data);
+    // Clear existing data
+    await db.signals.clear();
+
+    // Add new data
+    await db.signals.bulkAdd(data);
+
+  } catch (error) {
+    console.error('Error loading data into IndexedDB:', error);
+  }
+}
+
+// Export the function to be used elsewhere
+export { createDB, loadJSONToIndexedDB };
