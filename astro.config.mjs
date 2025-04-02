f924da0a7d93198d6eefb400ac307fa225d69d93
Elies <eliesgarciaalvira@gmail.com>
2025-04-02 12:00:45 +0200

    updated tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,21 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-  ],
   vite: {
     resolve: {
       alias: {
         '~/': '/src/'
       }
     },
+
     optimizeDeps: {
       include: ['lightweight-charts']
-    }
+    },
+
+    plugins: [tailwindcss()]
   }
 
 });
