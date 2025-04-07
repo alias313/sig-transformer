ff88c1d84ad007d3666a9f4ab357cace66c64d4c
Elies <eliesgarciaalvira@gmail.com>
2025-04-07 18:37:32 +0200

    Installed react and removed styles from InfoCard

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,29 @@
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-    resolve: {
-      alias: {
-        '@/': '/src/'
-      }
-    },
-
-    optimizeDeps: {
-      include: ['lightweight-charts']
-    },
-
-    plugins: [tailwindcss()]
+  vite: {
+  resolve: {
+    alias: {
+      '@/': '/src/'
+    }
   },
-	experimental: {
-		svg: true,
+
+  optimizeDeps: {
+    include: ['lightweight-charts']
+  },
+
+  plugins: [
+    tailwindcss(),
+  ]
+},
+
+  experimental: {
+      svg: true,
 	},
+
+  integrations: [react()],
 });
