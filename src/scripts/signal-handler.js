d31a048692c60b72954f611c9cf696c196184e27
Elies <eliesgarciaalvira@gmail.com>
2025-04-08 21:14:02 +0200

    VPS working

--- src/scripts/signal-handler.js
+++ src/scripts/signal-handler.js
@@ -48,7 +48,7 @@ console.log("Loaded parameters from LocalStorage:", signalParamsOnReload);
 async function transformSignal(signalParams) {
     // Collect form data to a plain object
     try {
-        const response = await fetch('http://localhost:3000/execute-fft', {
+        const response = await fetch('http://srv785333.hstgr.cloud:3000/execute-fft', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(signalParams)
